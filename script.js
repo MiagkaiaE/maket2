427583ee683676dff3f230976cec8c6cdf20a900
Elena Miagkaia <miagkaia.e@gmail.com>
2025-04-07 10:56:14 +0300

    Исправила пункты mob nav на ссылки, дописала JS для закрытия по ссылкам меню и Esc

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 
-const mobileNuvButtonOpen = document.querySelector('.header__mobile-nav');
+const mobileNavButtonOpen = document.querySelector('.header__mobile-nav');
 const mobileNav = document.querySelector('.mobile-nav');
-const mobileNuvButtonClose = document.querySelector('.mobile-nav__close')
+const mobileNavButtonClose = document.querySelector('.mobile-nav__close');
+const mobileNavLinks = document.querySelectorAll('.mobile-nav__item a');
 
+// проверка, что элементы существуют
+if (mobileNavButtonOpen && mobileNavButtonClose && mobileNav && mobileNavLinks) {
+	// меню открытие
+	mobileNavButtonOpen.addEventListener('click', () => {
+		mobileNav.classList.add('mobile-nav--open');
+		document.body.style.overflow = 'hidden'; // Блокируем скролл страницы
+	});
+	// закрытие по крестику
+	mobileNavButtonClose.addEventListener('click', () => {
+		mobileNav.classList.remove('mobile-nav--open');
+		document.body.style.overflow = ''; //возвращаем скрол по умолчанию
+	});
+	// закрытие при нажатии на пункт меню
+	mobileNavLinks.forEach(link => {
+		link.addEventListener('click', (event) => {
+			event.preventDefault(); // Отменяем стандартное поведение ссылки для одностраничника
+			mobileNav.classList.remove('mobile-nav--open');
+			document.body.style.overflow = ''; //возвращаем скрол по умолчанию
+		})
+	});
 
-mobileNuvButtonOpen.addEventListener('click', () => {
-	mobileNav.classList.add('mobile-nav__open');
-});
-
-mobileNuvButtonClose.addEventListener('click', () => {
-	mobileNav.classList.remove('mobile-nav__open');
-})
+	// Закрытие меню по клавише Esc
+	document.addEventListener('keydown', (event) => {
+		if (event.key === 'Escape' && mobileNav.classList.contains('mobile-nav__open')) {
+			mobileNav.classList.remove('mobile-nav__open');
+			document.body.style.overflow = ''; //возвращаем скрол по умолчанию
+		}
+	 });
+} else {
+	console.error('Один из элементов не найден: .header__mobile-nav, .mobile-nav или .mobile-nav__close');
+}
